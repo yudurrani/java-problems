802025029c24d22b90cb3a641328d59014fa5121
Yasir Durrani <yudurrani@gmail.com>
2019-03-02 19:28:42 -0500

    LinkedList

---
+++ src/datastructures/LinkedList.java
@@ -0,0 +1,216 @@
+package datastructures;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class LinkedList<E> implements List<E> {
+
+	Node<E> head;
+	Node<E> tail;
+	int size;
+
+	@Override
+	public boolean add(E e) {
+
+		if (size == 0) {
+			head = new Node<E>(e);
+			tail = head;
+			size++;
+			return true;
+		}
+
+		Node<E> temp = new Node<E>(e);
+		tail.next = temp;
+		temp.prev = tail;
+		tail = temp;
+		size++;
+
+		return true;
+	}
+
+	@Override
+	public void add(int index, E element) {
+
+		Node<E> curr = head;
+		if (index >= size) {
+			add(element);
+		}
+
+		for (int x = 0; x < size; x++) {
+
+			if (x == index) {
+
+				// we have reached a point where we want to add the new node
+
+				Node<E> temp = new Node<E>(element);
+
+				temp.next = curr;
+
+				temp.prev = curr.prev;
+				curr.prev = temp;
+				if (temp.prev != null) {
+
+					temp.prev.next = temp;
+				} else {
+					head = temp;
+				}
+				size ++ ;
+				return;
+			}
+
+			curr = curr.next;
+
+		}
+
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends E> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+		size =0 ;
+		head = null ;
+		tail = null ;
+
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E get(int index) {
+		// TODO Auto-generated method stub
+
+		Node<E> curr = head;
+
+		for (int x = 0; x < size; x++) {
+
+			if (x == index) {
+				return curr.value;
+			}
+
+			curr = curr.next;
+
+		}
+
+		return null;
+	}
+
+	@Override
+	public int indexOf(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return size ==0 ;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int lastIndexOf(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ListIterator<E> listIterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ListIterator<E> listIterator(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E remove(int index) {
+		// TODO Auto-generated method stub
+		
+		
+		return null;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E set(int index, E element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<E> subList(int fromIndex, int toIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/datastructures/Node.java
+++ src/datastructures/Node.java
@@ -4,7 +4,10 @@ public class Node<T> {
 	
 	T value;
 	Node<T> next;
-	
+	Node<T> prev;
+	Node(T value){
+		this.value=value;
+	}
 	Node(T value, Node<T> next){
 		this.value=value;
 		this.next=next;
