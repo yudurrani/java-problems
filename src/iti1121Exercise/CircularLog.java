c1eede88b144c0658d361c610fc82f77e3ec340a
Yasir Durrani <yudurrani@gmail.com>
2019-03-16 15:56:53 -0400

    CircularLogAgain

--- src/iti1121Exercise/CircularLog.java
+++ src/iti1121Exercise/CircularLog.java
@@ -3,8 +3,12 @@ package iti1121Exercise;
 
 public class CircularLog {
 
+	int n;
 
 	CircularLog(int maxCapacity) {
+		for ( int x = 0 ; x < maxCapacity; x++) {
+			
+		}
 	}
 
 	public void log(String info) {
