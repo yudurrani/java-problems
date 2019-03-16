e930d8028ee28435f5067999d881fce2fb52de55
Yasir Durrani <yudurrani@gmail.com>
2019-03-15 20:59:17 -0400

    CircularLogIteratorFinal

--- src/iti1121Exercise/CircularLog.java
+++ src/iti1121Exercise/CircularLog.java
@@ -1,58 +1,17 @@
 package iti1121Exercise;
 
-import datastructures.Queue;
 
-public class CircularLog implements Queue{
+public class CircularLog {
+
+
+	CircularLog(int maxCapacity) {
+	}
+
+	public void log(String info) {
+		
+	}
 	
-	int[] array;
-
-	int total;
-
-	CircularLog() {
-	 array = new int[4];
-	}
-
-	@Override
-	public void enqueue(int i) {
-		
-		if ( array.length == 4 ) {
-			
-			while ( array[3] == array[2])
-			
-			for (int x=0; x<3 ; x++) {
-				array[x]= array[x+1];
-			}
-			array[3] = i ;
-		}
-		
-		else {
-		array[total] = i;
-		total++;
-		}
-		
-		
-	}
-	public int dequeue() {
-		int tmp = array[0];
-
-		for (int x = 0; x < total; x++) {
-			array[x] = array[x + 1];
-		}
-		total--;
-		return tmp;
-
-	}
-
-	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return total;
+		return 0;
 	}
-
-	@Override
-	public boolean isEmpty() {
-
-		return total == 0;
-	}
-
 }
---
+++ src/iti1121Exercise/CircularLogIterator.java
@@ -0,0 +1,19 @@
+package iti1121Exercise;
+
+import java.util.Iterator;
+
+public class CircularLogIterator implements Iterator{
+
+	@Override
+	public boolean hasNext() {
+		 
+		return false;
+	}
+
+	@Override
+	public Object next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
