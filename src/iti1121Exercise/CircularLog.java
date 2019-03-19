984069add2fc5a74e0cb8dd9767626ed49f798da
Yasir Durrani <yudurrani@gmail.com>
2019-03-18 21:03:09 -0400

    CircularLogComplete

--- src/iti1121Exercise/CircularLog.java
+++ src/iti1121Exercise/CircularLog.java
@@ -1,21 +1,44 @@
 package iti1121Exercise;
 
-
 public class CircularLog {
 
-	int n;
+	int capacity;
+	int size;
+	int curser;
+	String[] forInfo;
 
 	CircularLog(int maxCapacity) {
-		for ( int x = 0 ; x < maxCapacity; x++) {
-			
-		}
+		capacity = maxCapacity;
+		forInfo = new String[maxCapacity];
+
 	}
 
 	public void log(String info) {
-		
+
+		if (curser == capacity) {
+			curser = 0;
+		}
+		forInfo[curser] = info;
+		curser++;
+		size++;
+
 	}
-	
+
 	public int size() {
-		return 0;
+		if (size < capacity) {
+			return size;
+		} else
+			return capacity;
 	}
+
+	public String get(int x) {
+
+		if (size < capacity) {
+			return forInfo[x];
+		} else
+			return forInfo[(curser+x+1)%capacity];
+
+
+	}
+
 }
