3322ab1c5548bb132c47f46d1ddb22b3ba419ee8
Yasir Durrani <yudurrani@gmail.com>
2019-03-25 20:52:31 -0400

    DeckGameofRummy

--- src/gameOfRumyy/Deck.java
+++ src/gameOfRumyy/Deck.java
@@ -1,7 +1,42 @@
 package gameOfRumyy;
 
+import java.util.ArrayList;
+
+
 public class Deck {
 	
+	int totalCards;
+	ArrayList<Card> cards ;
+	
+	
+	
+	public Deck() {
+		cards = new ArrayList<Card>();
+		
+	}
+	
+	public Deck(int n) {
+		totalCards = 4 * n ;
+		cards = new ArrayList<Card>();
+		
+	}
+	
+	public int size() {
+		return totalCards;
+	}
+	
+	public boolean hasCards() {
+		return totalCards != 0 ;
+	}
+	
+	public Card get(int pos) {
+		return cards.get(pos-1)  ;
+	}
 	// use arrayList
+	
+	public void add (Card card) {
+		cards.add(card) ;
+		totalCards++ ;
+	}
 
 }
