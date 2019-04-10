5188b17f362b2095572da8909ca0877d01659ff4
Yasir Durrani <yudurrani@gmail.com>
2019-04-10 01:23:48 -0400

    UpdatedDeck

--- src/gameOfRumyy/MainGame.java
+++ src/gameOfRumyy/MainGame.java
@@ -16,10 +16,7 @@ public class MainGame {
 
 		handDeck = new Deck();
 
-		for (int i = 0; i < 7; i++) {
-
-			handDeck.add(newDeck.removeLast());
-		}
+		handDeck.addAll(newDeck.deal(7));
 
 		die = new Die();
 
@@ -27,5 +24,50 @@ public class MainGame {
 
 	public void play() {
 
+		while (handDeck.size() != 0) {
+
+			if (die.roll() == 1) {
+				handDeck.removeFirst();
+
+			} else {
+				int newCards = Math.min(die.getValue(), newDeck.size());
+				handDeck.addAll(newDeck.deal(newCards));
+
+				handDeck.sortByRank();
+				Deck sameRank = new Deck();
+
+				Card previousCard = handDeck.get(0);
+				sameRank.add(previousCard);
+
+				for (int i = 1; i < handDeck.size(); i++) {
+					
+					Card currentCard = handDeck.get(i);
+					
+					
+					if (currentCard.equals(previousCard)) {
+						sameRank.add(currentCard);
+						
+					} else {
+						if (sameRank.isKind()) {
+							handDeck.removeAll(sameRank);
+						}
+						
+						sameRank = new Deck();
+						previousCard = currentCard;
+						sameRank.add(previousCard);
+					}
+					
+				}
+				handDeck.sortBySuit();
+				
+				for ( int i =0; i < handDeck.size(); i++) {
+					if (handDeck.isSeq()) {
+						
+					}
+					
+				}
+			}
+		}
 	}
+
 }
