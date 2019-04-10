cd9169c42d3ecd7feb9fe7fef4d55aaacbe6c8c8
Yasir Durrani <yudurrani@gmail.com>
2019-04-09 20:42:04 -0400

    UpdatedMainGame

--- src/gameOfRumyy/MainGame.java
+++ src/gameOfRumyy/MainGame.java
@@ -3,38 +3,54 @@ package gameOfRumyy;
 import java.util.ArrayList;
 
 public class MainGame {
+	
 
+	
 	/*
 	 * Our implementation of the game consists of 6 classes: Die, Card, Deck, Game,
 	 * Run, and Utils. In the following sections, we describe the requirements for
 	 * each class
 	 */
-
+	
 	public static void main(String[] args) {
-
-		Die d;
-		d = new Die();
-		System.out.println(d.getValue());
-		d.roll();
-		System.out.println(d.getValue());
-		System.out.println(d);
-
-		Card a, b, c;
-		a = new Card(2, 7);
-		b = new Card(2, 8);
-		c = new Card(2, 9);
-		System.out.println(a);
-		System.out.println(a.equals(b));
-		System.out.println(a.equals(c));
-
-		Deck newDeck = new Deck();
 		
-		newDeck.add(a);
-		newDeck.add(b);
-		newDeck.add(c);
+	
+	Deck newDeck = new Deck(6);
+	
+	newDeck.shuffle();
+	
+	Deck handDeck = new Deck();
+	
+	Die die = new Die();
+	
+	
 
-		System.out.println(newDeck.isSeq());
+
+//
+//		Die d;
+//		d = new Die();
+//		System.out.println(d.getValue());
+//		d.roll();
+//		System.out.println(d.getValue());
+//		System.out.println(d);
+//
+//		Card a, b, c;
+//		a = new Card(2, 7);
+//		b = new Card(2, 8);
+//		c = new Card(2, 9);
+//		System.out.println(a);
+//		System.out.println(a.equals(b));
+//		System.out.println(a.equals(c));
+//
+//		Deck newDeck = new Deck();
+//		
+//		newDeck.add(a);
+//		newDeck.add(b);
+//		newDeck.add(c);
+//
+//		System.out.println(newDeck.isSeq());
+//
+//	}
 
 	}
-
 }
